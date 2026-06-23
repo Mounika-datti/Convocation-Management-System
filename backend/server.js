47dc81d81f8d4302ad85d28494cd7a1b677a2996
Mounika Datti <mounikadatti05@gmail.com>
2026-06-23 14:03:05 +0530

    Update CORS for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,15 @@ const app = express();
 // Middlewares
 // ======================
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://convocation-management-system.vercel.app/"
+    ],
+    credentials: true,
+  })
+); 
 
 app.use(express.json());
 
